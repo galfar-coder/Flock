2cfae4b7aaff79988ed2a0e0a960337d98aaca95
Topeez <topetopinka7@seznam.cz>
2025-02-12 23:36:17 +0100

    base layout done

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cons = require("consolidate");
+var livereload = require("livereload");
+var connectLiveReload = require("connect-livereload");
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
+
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -11,6 +22,8 @@ var app = express();
 app.engine("html", cons.swig);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "html");
+
+app.use(connectLiveReload());
 
 app.use(logger("dev"));
 app.use(express.json());
